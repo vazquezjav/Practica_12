92d6ad2e0b7309532978263579a2adc0712262b2
Estudiante <Estudiante@lab0119>
2017-12-06 09:04:52 -0500

    practica 12 Hola mundo

---
+++ Practica_12/src/controlador/Gestion.java
@@ -0,0 +1,54 @@
+package controlador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Articulo;
+import modelo.Autor;
+import modelo.Revista;
+
+public class Gestion {
+	private List<Revista> revistas;
+	private List<Articulo> articulos;
+	private List<Autor> autores;
+	
+	public Gestion(){
+		revistas= new ArrayList<Revista>();
+		articulos= new ArrayList<Articulo>();
+		autores= new ArrayList<Autor>();		
+	}
+	
+	public void agregarArticulo(String nombreA, String apellido, String nacionalidad, String temaAr, String idioma){
+		Autor au= new Autor();
+		au.setNombre(nombreA);
+		au.setApellido(apellido);
+		au.setNacionalidad(nacionalidad);
+		autores.add(au);
+		
+		Articulo ar= new Articulo();
+		ar.setIdioma(idioma);
+		ar.setTema(temaAr);
+		ar.setAutor(au);
+		
+		articulos.add(ar);
+	}
+	
+	
+	public void agregarRevista(String nombreR, String editorial, Articulo ar){
+		Revista re= new Revista();
+		re.setNombre(nombreR);
+		re.setEditorial(editorial);
+		re.setArticulo(ar);
+		
+		revistas.add(re);
+	}
+	public boolean buscarAu(String nombre, String apellido, String nacionalidad)throws Exception{
+		for(Autor au:autores){
+			if(au.getApellido().equals(apellido)){
+				
+			}
+		}
+		return true;
+	}
+	
+}
