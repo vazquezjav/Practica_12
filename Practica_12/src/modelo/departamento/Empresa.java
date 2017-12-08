3f25878485a9c6dfe6964c651a45d6402e426a64
Estudiante <Estudiante@lab0414>
2017-12-08 08:50:49 -0500

    actualizacion 08/12/2017

--- Practica_12/src/modelo/departamento/Empresa.java
+++ Practica_12/src/modelo/departamento/Empresa.java
@@ -1,5 +1,41 @@
 package modelo.departamento;
 
 public class Empresa {
+	private String nombre;
+	private String ruc;
+	private String direccion;
+	private Departamento departamentos;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getRuc() {
+		return ruc;
+	}
+
+	public void setRuc(String ruc) {
+		this.ruc = ruc;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Departamento getDepartamentos() {
+		return departamentos;
+	}
+
+	public void setDepartamentos(Departamento departamentos) {
+		this.departamentos = departamentos;
+	}
 
 }
