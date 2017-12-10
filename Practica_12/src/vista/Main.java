e9583516587cd22bf2632b97f613133a95888388
javier <javier@javier>
2017-12-09 22:50:57 -0500

    a

--- Practica_12/src/vista/Main.java
+++ Practica_12/src/vista/Main.java
@@ -7,14 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub]
 		
-//
-//		MiVentana mi= new MiVentana();
-//		mi.setVisible(true);
+
+		MiVentana mi= new MiVentana();
+		mi.setVisible(true);
 		
-		GestionRevista gd= new GestionRevista();
-		
-		VisRevista v= new VisRevista(gd);
-		v.setVisible(true);
+//		GestionRevista gd= new GestionRevista();
+//		
+//		VisRevista v= new VisRevista(gd);
+//		v.setVisible(true);
 			
 	}
 
